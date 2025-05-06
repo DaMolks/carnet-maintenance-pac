75e156ba30b743f8bb370940507dc78af3d4c865
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-05-06 15:26:05 +0200

    Ajout du composant App principal

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CarnetMaintenancePAC from './components/CarnetMaintenancePAC';
+
+function App() {
+  return (
+    <div className="App">
+      <CarnetMaintenancePAC />
+    </div>
+  );
+}
+
+export default App;
